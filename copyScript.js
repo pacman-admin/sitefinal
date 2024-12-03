f6cf32818e2c7c378c942cb7931dfb97d3ae84fb
pacman-admin <118117362+pacman-admin@users.noreply.github.com>
2024-12-03 16:56:43 +0000

    Made files smaller

--- copyScript.js
+++ copyScript.js
@@ -1,14 +1,6 @@
-function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("myInput");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+function f() {
+  var a = document.getElementById("myInput");
+  a.select();
+  a.setSelectionRange(0, 64);
+  navigator.clipboard.writeText(a.value);
 } 
